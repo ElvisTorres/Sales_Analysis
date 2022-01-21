852dadc6dd799ad1dfcbfa4c47cf4d75c2861a92
ElvisTorres <78370769+ElvisTorres@users.noreply.github.com>
2022-01-21 15:28:07 -0400

    Add files via upload

--- Product Sales Analysis.sql
+++ Product Sales Analysis.sql
@@ -1,36 +1,43 @@
-DROP TABLE product_sales;
+DROP TABLE IF EXISTS product_sales;
 /*Drops the table in case it already exists*/
 
+/*Table Schems*/
 CREATE TABLE product_sales(
-	Retailer_Country VARCHAR (200),
-    Order_Method_Type VARCHAR (200),
-    Retailer_Type VARCHAR (200),
-    Product_Line VARCHAR (200),
-    Product_Type VARCHAR (200),
-    Product VARCHAR (200),
+	Retailer_Country VARCHAR (255),
+    Order_Method_Type VARCHAR (255),
+    Retailer_Type VARCHAR (255),
+    Product_Line VARCHAR (255),
+    Product_Type VARCHAR (255),
+    Product VARCHAR (255),
     Year_Purchase INT,
-    Quarter_Purchase VARCHAR (200),
+    Quarter_Purchase VARCHAR (255),
     Revenue INT,
     Quantity INT,
     Gross_Margin INT NULL
     );
-    
+ 
+ /*Loads data from a csv file*/
 LOAD DATA INFILE 'ibm_product_sales.csv' INTO TABLE product_sales
     FIELDS TERMINATED BY ','
     ENCLOSED BY '\"' 
     LINES TERMINATED BY '\n' 
     IGNORE 1 LINES;
 
+/*Just to check that everything loaded correctly*/
 SELECT * FROM product_sales;
+
+/*Removes rows with null revenue*/
+DELETE FROM product_sales
+WHERE Revenue IS NULL;
 
 /*Gives the revenue and quantity of products sold per year*/
 SELECT Year_Purchase, SUM(Revenue), SUM(Quantity) FROM product_sales
 GROUP BY Year_Purchase;
 
 /*Gives the revenue and quantity of products sold per quarter*/
-SELECT Quarter_Purchase, SUM(Revenue), SUM(Quantity) FROM product_sales
+SELECT Quarter_Purchase, SUM(Revenue), SUM(Quantity), Year_Purchase FROM product_sales
 GROUP BY Quarter_Purchase
-ORDER BY Quarter_Purchase;
+ORDER BY Year_Purchase, Quarter_Purchase;
 
 /*Gives the revenue by country*/
 SELECT Retailer_Country, SUM(Revenue) FROM product_sales
@@ -41,15 +48,21 @@ ORDER BY SUM(Revenue) DESC;
 SELECT Retailer_Country, SUM(Revenue), Year_Purchase FROM product_sales
 GROUP BY Retailer_Country,Year_Purchase
 ORDER BY Retailer_Country;
-/*Convert this into a pivot table*/
-
-/*Gives the revenue by country by Year of the Top 5 countries with highest revenue
-  using subqueries*/
+/*TO DO: Convert this into a pivot table*/
 
 /*Gives the revenue by product line*/
 SELECT DISTINCT(Product_Line), AVG(Revenue) FROM product_sales
 GROUP BY Product_Line
 ORDER BY AVG(Revenue) DESC;
 
-/*Golf equipment was the product line with largest revenue.
-This gives the list of products and their avg revenue in that product line*/
+/*Gives the top five most selling products*/
+SELECT DISTINCT(Product), SUM(Revenue), SUM(Quantity) FROM product_sales
+GROUP BY Product
+ORDER BY SUM(Quantity) DESC
+LIMIT 5;
+
+/*Gives the Average Yearly Revenue (using subqueries)*/
+SELECT AVG(Sum_Table) FROM (
+	SELECT Year_Purchase, SUM(Revenue) AS Sum_Table FROM product_sales
+    GROUP BY Year_Purchase) 
+Avg_Table;
